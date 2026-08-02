c011d976b0783f540926c55d507f0a66625c12fe
kimtrucU <ungkimtruc2006@gmail.com>
2026-08-02 22:05:09 +0700

    Fix CSS HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         Paginator::useBootstrapFive();
 
         // Share cart count globally with all views
